521214a972de10a80bbb6647bb9f060b83c8114c
Bew <Bew@sysops-MBP.broadband>
2020-12-18 15:20:34 +0000

    Create a types file to hold all the actions (right now is only one) but good to organise it this way

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
+import { SONG_SELECTED } from 'actions/types';
+
 export const selectSong = (song) => {
   return {
-    type: 'SONG_SELECTED',
+    type: SONG_SELECTED,
     payload: song,
   };
 };
