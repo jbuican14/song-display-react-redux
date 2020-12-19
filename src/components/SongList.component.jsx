52c1c9d4eb241ffcd447c6a09cde9d26ff836fe6
Bew <Bew@sysops-MBP.broadband>
2020-12-19 21:37:10 +0000

    Wire up selectSong action to the Redux

--- src/components/SongList.component.jsx
+++ src/components/SongList.component.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
 import { connect } from 'react-redux';
+import { selectSong } from 'actions';
 
 class SongList extends Component {
   renderList() {
     return this.props.songs.map((item, idx) => {
       return (
         <div className="item" key={idx}>
-          <button>Select</button>
+          <button onClick={() => this.props.selectSong(item)}>Select</button>
           <div className="content">{item.title}</div>
         </div>
       );
@@ -14,7 +16,7 @@ class SongList extends Component {
   }
 
   render() {
-    // console.log(this.props.songs);
+    console.log(this.props);
     return (
       <div>
         <h1>Song List</h1>
@@ -25,8 +27,8 @@ class SongList extends Component {
 }
 
 const mapStateToProps = (state) => {
-  // console.log(state);
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong: selectSong })(SongList);
